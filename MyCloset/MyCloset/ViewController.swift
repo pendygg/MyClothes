13ccab9e4b573a3842b77deec7b4612f43a280cb
012_100006640936_pendygg <012_100006640936_pendygg@git.cloud.tencent.com>
2019-03-24 20:13:35 +0900

    init project

---
+++ MyCloset/MyCloset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCloset
+//
+//  Created by Teki Hou on 2019/03/24.
+//  Copyright © 2019 Teki Hou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
